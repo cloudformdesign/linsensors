06b1c9e31192ff9c92b396c16212e6c77ac9b826
Garrett Berg <gberg@neutral.io>
2015-03-30 10:47:07 -0600

    update htu to use i2cdev

--- linsensors/htu21d.py
+++ linsensors/htu21d.py
@@ -36,178 +36,85 @@ print(sensor.humidity)
 
 '''
 
-import fcntl
-import io
-import struct
 import time
+import array
 
-from . import sensorbase
+from i2cdev import I2C
 
-# fcntl
-_I2C_SLAVE = 0x0703
+I2C_SLAVE = 0x0703
+HTU21D_ADDR = 0x40
+CMD_READ_TEMP_HOLD = b"\xE3"
+CMD_READ_HUM_HOLD = b"\xE5"
+CMD_READ_TEMP_NOHOLD = b"\xF3"
+CMD_READ_HUM_NOHOLD = b"\xF5"
+CMD_WRITE_USER_REG = b"\xE6"
+CMD_READ_USER_REG = b"\xE7"
+CMD_SOFT_RESET = b"\xFE"
 
-# Default I2C address
-_DEFAULT_ADDRESS = 0x40
 
-# Configuration parameters
-RESOLUTION_12BITS      = 0b00000000
-RESOLUTION_8BITS       = 0b00000001
-RESOLUTION_10BITS      = 0b10000000
-RESOLUTION_11BITS      = 0b10000001
-# _END_OF_BATTERY        = 0b01000000
-# _ENABLE_ONCHIP_HEATER  = 0b00000100
-_DISABLE_ONCHIP_HEATER = 0b00000000
-_ENABLE_OTP_RELOAD     = 0b00000000
-_DISABLE_OTP_RELOAD    = 0b00000010
-_RESERVED_BITMASK      = 0b00111000
+class HTU21D(object):
+    def __init__(self, address):
+        self.dev = I2C(HTU21D_ADDR, 1, address)  # HTU21D 0x40, bus 1
+        with self.dev.lock:
+            self.dev.write(CMD_SOFT_RESET)  # soft reset
+        time.sleep(.1)
 
-# Commands
-_CMD_TEMPERATURE  = '\xF3'
-_CMD_HUMIDITY     = '\xF5'
-_CMD_WRITE_CONFIG = '\xE6'
-_CMD_READ_CONFIG  = '\xE7'
-_CMD_SOFT_RESET   = '\xFE'
+    def ctemp(self, sensorTemp):
+        tSensorTemp = sensorTemp / 65536.0
+        return -46.85 + (175.72 * tSensorTemp)
 
-# Data bits specification
-_STATUS_BITMASK     = 0b00000011
-_STATUS_TEMPERATURE = 0b00000000
-_STATUS_HUMIDITY    = 0b00000010
-_STATUS_LSBMASK     = 0b11111100
+    def chumid(self, sensorHumid):
+        tSensorHumid = sensorHumid / 65536.0
+        return -6.0 + (125.0 * tSensorHumid)
 
-class Htu21d(sensorbase.SensorBase):
-    def __init__(self, bus = None, addr = _DEFAULT_ADDRESS,
-                 resolution = RESOLUTION_12BITS,
-                 use_temperature = False):
-        '''Initializes the sensor with some default values.
+    def crc8check(self, value):
+        # Ported from Sparkfun Arduino HTU21D Library: https://github.com/sparkfun/HTU21D_Breakout
+        remainder = ((value[0] << 8) + value[1]) << 8
+        remainder |= value[2]
 
-        bus: The SMBus descriptor on which this sensor is attached.
-        addr: The I2C bus address
-            (default is 0x40).
+        # POLYNOMIAL = 0x0131 = x^8 + x^5 + x^4 + 1
+        # divsor = 0x988000 is the 0x0131 polynomial shifted to farthest left of three bytes
+        divsor = 0x988000
 
-        '''
-        assert(bus is not None)
-        assert(addr > 0b0000111
-               and addr < 0b1111000)
-        assert(resolution == RESOLUTION_12BITS
-               or resolution == RESOLUTION_8BITS
-               or resolution == RESOLUTION_10BITS
-               or resolution == RESOLUTION_11BITS)
-        assert(use_temperature == True
-               or use_temperature == False)
+        for i in range(0, 16):
+            if(remainder & 1 << (23 - i)):
+                remainder ^= divsor
+            divsor = divsor >> 1
 
-        super(Htu21d, self).__init__(self._update_sensor_data)
+        if remainder == 0:
+            return True
+        else:
+            return False
 
-        self._ior = io.open('/dev/i2c-' + str(bus), 'rb', buffering=0)
-        self._iow = io.open('/dev/i2c-' + str(bus), 'wb', buffering=0)
-        fcntl.ioctl(self._ior, _I2C_SLAVE, addr)
-        fcntl.ioctl(self._iow, _I2C_SLAVE, addr)
+    def read_temperature(self):
+        with self.dev.lock:
+            self.dev.write(CMD_READ_TEMP_NOHOLD)  # measure temp
+            time.sleep(.1)
 
-        self._resolution = resolution
-        self._onchip_heater = _DISABLE_ONCHIP_HEATER
-        self._otp_reload = _DISABLE_OTP_RELOAD
+            data = self.dev.read(3)
+        buf = array.array('B', data)
 
-        self._humidity = None
-        self._temperature = None
-        self._use_temperature = use_temperature
+        if self.crc8check(buf):
+            temp = (buf[0] << 8 | buf[1]) & 0xFFFC
+            return self.ctemp(temp)
+        else:
+            return -255
 
-        self._reset()
-        self._reconfigure()
+    def read_humidity(self):
+        with self.dev.lock:
+            self.dev.write(CMD_READ_HUM_NOHOLD)  # measure humidity
+            time.sleep(.1)
 
-    @property
-    def humidity(self):
-        '''Returns a relative humidity value.  Returns None if no valid value
-        is set yet.
+            data = self.dev.read(3)
+        buf = array.array('B', data)
 
-        '''
-        self._update()
-        return (self._humidity)
+        if self.crc8check(buf):
+            humid = (buf[0] << 8 | buf[1]) & 0xFFFC
+            return self.chumid(humid)
+        else:
+            return -255
 
-    @property
-    def temperature(self):
-        '''Returns a temperature value.  Returns None if no valid value is set
-        yet.
-
-        '''
-        if self._use_temperature is True:
-            self._update()
-        return (self._temperature)
-
-    @property
-    def use_temperature(self):
-        '''Returns whether to measure temperature or not.
-
-        '''
-        return (self._use_temperature)
-
-    @use_temperature.setter
-    def use_temperature(self, use_temperature):
-        assert(use_temperature == True
-               or use_temperature == False)
-        self._use_temperature = use_temperature
-
-    @property
-    def resolution(self):
-        '''Gets/Sets the resolution of temperature value.
-
-        RESOLUTION_12BITS: RH 12 bits, Temp 14 bits.
-        RESOLUTION_8BITS:  RH  8 bits, Temp 12 bits.
-        RESOLUTION_10BITS: RH 10 bits, Temp 13 bits.
-        RESOLUTION_11BITS: RH 11 bits, Temp 11 bits.
-
-        '''
-        return (self._resolution)
-
-    @resolution.setter
-    def resolution(self, resolution):
-        assert(resolution == RESOLUTION_12BITS
-               or resolution == RESOLUTION_8BITS
-               or resolution == RESOLUTION_10BITS
-               or resolution == RESOLUTION_11BITS)
-        self._resolution = resolution
-        self._reconfigure()
-
-    def _reset(self):
-        self._iow.write(_CMD_SOFT_RESET)
-        time.sleep(0.02)
-
-    def _reconfigure(self):
-        self._iow.write(_CMD_READ_CONFIG)
-        configs = self._ior.read(1)
-        (config,) = struct.unpack('B', configs)
-        config = ((config & _RESERVED_BITMASK)
-                  | self._resolution
-                  | self._onchip_heater
-                  | self._otp_reload)
-        self._iow.write(_CMD_WRITE_CONFIG + struct.pack('B', config))
-
-    def _update_sensor_data(self):
-        if self._use_temperature is True:
-            self._iow.write(_CMD_TEMPERATURE)
-            time.sleep(0.05)
-            vals = self._ior.read(3)
-            (temphigh, templow, crc) = struct.unpack('BBB', vals)
-            temp = (temphigh << 8) | (templow & _STATUS_LSBMASK)
-            self._temperature = -46.85 + (175.72 * temp) / 2**16
-
-        self._iow.write(_CMD_HUMIDITY)
-        time.sleep(0.02)
-        vals = self._ior.read(3)
-        (humidhigh, humidlow, crc) = struct.unpack('BBB', vals)
-        humid = (humidhigh << 8) | (humidlow & _STATUS_LSBMASK)
-        self._humidity = -6 + (125.0 * humid) / 2**16
-
-if __name__ == '__main__':
-
-    bus = 1
-    sensor = Htu21d(bus)
-    for cache in [0, 5]:
-        print('**********')
-        print('Cache lifetime is %d' % cache)
-        sensor.cache_lifetime = cache
-        for c in range(10):
-            for res in [RESOLUTION_12BITS,
-                        RESOLUTION_8BITS,
-                        RESOLUTION_10BITS,
-                        RESOLUTION_11BITS]:
-                sensor.resolution = res
-                print(sensor.humidity, sensor.temperature)
+if __name__ == "__main__":
+    obj = HTU21D(I2C_SLAVE)
+    print("Temp:", obj.read_temperature(), "C")
+    print("Humid:", obj.read_humidity(), "% rH")
