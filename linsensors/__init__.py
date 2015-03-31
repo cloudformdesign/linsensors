9b638f8a9af938cc556ec6136188fc0ce0ea11e5
Garrett <gberg@n.io>
2015-03-31 17:40:07 +0000

    version 0.0.2

--- linsensors/__init__.py
+++ linsensors/__init__.py
@@ -24,7 +24,7 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 import glob
 import re
